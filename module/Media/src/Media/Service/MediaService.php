e7688bdc4cf57e47566e3fb20e85f48b9d949ae0
salt42 <boluuuu@gmail.com>
2016-10-13 16:20:55 +0200

    add jsTree

--- module/Media/src/Media/Service/MediaService.php
+++ module/Media/src/Media/Service/MediaService.php
@@ -6,9 +6,35 @@ class MediaService {
     function __construct() {
     }
 
-
-
     function import() {
         //check import folder for files, and if any, import them.
+        //if gallery folder has a sub folder check if this folder exists allready
+        //then move content images to storage
+        //if not move folder and add permission
+        //clear import folder
+    }
+
+    /**
+     * returns a multidimensional array with folders
+     */
+    function getAllFolder() {
+        return array(
+            'name',
+            'path',
+            'folders' => array(
+                'name',
+                'path',
+                'folders' => array())
+        );
+    }
+    /**
+     * @param {string} $path relative path to the data folder
+     */
+    function getFolder($path) {
+        //path exists
+        //load folder.perm
+        //check permission   permission template = recource is media  privileg is /path/to/folder
+        //if no permission ?? redirect to login ?? or return false
+        //return array with file paths'
     }
 }
