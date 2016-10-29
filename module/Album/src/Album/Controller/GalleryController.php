82312903a28312b339a1c23cfee3d0ffa4de02b7
salt42 <boluuuu@gmail.com>
2016-10-29 18:21:41 +0200

    fix gallery and know with own files :)

--- module/Album/src/Album/Controller/GalleryController.php
+++ module/Album/src/Album/Controller/GalleryController.php
@@ -8,9 +8,15 @@ use Album\Form\AlbumForm;
 
 class GalleryController extends AbstractActionController
 {
-
     protected $albumTable;
 
+    protected function getAlbumTable()
+    {
+        if (!$this->albumTable) {
+            $this->albumTable = $this->getServiceLocator()->get('Album\Model\AlbumTable');
+        }
+        return $this->albumTable;
+    }
 //    private function findedenfehler ()
 //    {
 //        if (property_exists ($this, 'getAlbumTable()') )
@@ -25,28 +31,20 @@ class GalleryController extends AbstractActionController
 
     public function indexAction()
     {
-        //so holste dir nen service im controller
-        $t = $this->getServiceLocator()->get('MediaService');
-        //die funktion importiert alle files aus Upload und giebt ein array mit errors zurück ... im mom nur wenn ein file bereits existiert
-        //eventuel bau ich in die import auch noch ein das thumbs erzeugt werden eventuel auch wo anders
-        //wenn du willst kannst du dem MediaService auch ne private funktion machen die alle gallerys durchschaut und thumbs macht wo keine sind
-        $t->import();
-        //getAlbumFiles giebt die alle images zurück die halt in dem ordner sind(image is nur ein array mit path,name und so)
-        $images = $t->getAlbumFiles('2016');
-        print('<pre>');
-        var_dump($images);
-        print('</pre>');
         $viewModel = new ViewModel(array(
-//            'test' => $this->findedenfehler(),
-//            'albums' => $this->getAlbumTable()->fetchAll()
+            'albums' => $this->getAlbumTable()->fetchAll()
         ) );
         return $viewModel;
     }
 
     public function smallAction()
     {
-        $viewModel = new ViewModel(array());
-            $viewModel->setTemplate('Album/gallery/small.phtml');
+        $t = $this->getServiceLocator()->get('MediaService');
+        $images = $t->getAlbumFiles('2016');
+        $viewModel = new ViewModel(array(
+            'images' => $images
+        ));
+        $viewModel->setTemplate('Album/gallery/small.phtml');
         return $viewModel;
     }
 
