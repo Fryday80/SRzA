f34e7b782e10366fa13e2383788287a58f19303d
Fryday80 <Fry.LMU@gmx.de>
2016-11-21 23:00:10 +0100

    prepare for render function  sraForm->renderNew()

---
+++ module/Application/src/Application/DataTable/AbstractDataTable.php
@@ -0,0 +1,14 @@
+<?php
+namespace Application\DataTable;
+/**
+ * Created by PhpStorm.
+ * User: Fry
+ * Date: 21.11.2016
+ * Time: 17:03
+ */
+class AbstractDataTable
+{
+    public function add($conf) {
+        //@todo imp
+    }
+}
