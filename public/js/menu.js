5cb9c49ccc1ffab7012f52dc5c6db37636f9d0fe
Fryday80 <Fry.LMU@gmx.de>
2017-01-24 18:12:24 +0100

    menu fixed ... again
    minor design fixes
    in css and js
    
    menu.js
    better readability

--- public/js/menu.js
+++ public/js/menu.js
@@ -33,29 +33,61 @@ $(document).ready (function menu_handler_js () {
     function runL () {
         $(".menu_closed").off("click", menuToggle);
 
-        $(".menuItems").removeClass("hidden")
+        /** style class changes **/
+        $(".menuItems")
+            .removeClass("hidden")
             .removeClass("s_Show");
-        $(".navbutton").not("hidden").addClass("hidden");
-        $(".navtitel").removeClass ("hidden");
-        $(".level_0 ul").removeClass("positionRelative sub_level_ul");
-        $(".level_0 li").removeClass("displayBlock");
-        $(".level_0").removeClass("li_S_view level_0_S")
+
+        $(".navbutton")
+            .not("hidden").addClass("hidden");
+
+        $(".navtitel")
+            .removeClass ("hidden");
+
+        $(".level_0 ul")
+            .removeClass("positionRelative")
+            .removeClass("sub_level_ul");
+
+        $(".level_0 li")
+            .removeClass("displayBlock")
+            .removeClass("li_border_lr");
+
+        $(".level_0")
+            .removeClass("li_S_view level_0_S")
             .not("level_0_animated").addClass("level_0_animated");
-        $(".navigation-background").removeClass("nbg");
+
+        $(".navigation-background")
+            .removeClass("nbg");
     }
 
     function runS () {
         $(".menu_closed").off("click", menuToggle);
 
-        $(".menuItems").not("hidden").addClass ("hidden")
+        /** style class changes **/
+        $(".menuItems")
+            .not("hidden").addClass ("hidden")
             .removeClass("s_Show");
-        $(".navbutton").removeClass("hidden");
-        $(".navtitel").not("hidden").addClass ("hidden");
-        $(".level_0 ul").not("positionRelative sub_level_ul").addClass("positionRelative sub_level_ul");
-        $(".level_0 li").not("displayBlock").addClass("displayBlock");
-        $(".level_0").not("li_S_view level_0_S").addClass("li_S_view level_0_S")
+
+        $(".navbutton")
+            .removeClass("hidden");
+
+        $(".navtitel")
+            .not("hidden").addClass ("hidden");
+
+        $(".level_0 ul")
+            .not("positionRelative").addClass("positionRelative")
+            .not("sub_level_ul").addClass("sub_level_ul");
+
+        $(".level_0 li")
+            .not("displayBlock").addClass("displayBlock")
+            .not("li_border_lr").addClass("li_border_lr");
+
+        $(".level_0")
+            .not("li_S_view level_0_S").addClass("li_S_view level_0_S")
             .removeClass("level_0_animated");
-        $(".navigation-background").not("nbg").addClass("nbg");
+
+        $(".navigation-background")
+            .not("nbg").addClass("nbg");
     }
 
     function menuActionsS () {
