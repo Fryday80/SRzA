3d71221cc7b63c5445dd649de619b7335b236170
Fryday80 <Fry.LMU@gmx.de>
2017-01-24 18:25:00 +0100

    menu.js commented

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,4 +1,4 @@
-/** help:
+/** help - Menu structure:
  * div  navframe
  *     div .navbutton
  *     /div
@@ -17,20 +17,31 @@
  *     /div
  */
 
-
 $(document).ready (function menu_handler_js () {
 
+/* ------------------ USED FUNCTIONS -------------------- */
+    /**
+     * adds the link images to all <a>
+     */
     function addLinkDecorators () {
         $(".bodycontainer a").not("#DataTables_Table_0_paginate a").prepend('<img class="links" src="/img/uikit/link.png">');
     }
-    
+
+    /**
+     * performs the menu show-hide action
+     */
     function menuToggle() {
         console.log ("hier");
         $(".menuItems").toggleClass("hidden");
         $(".menuItems").toggleClass("s_Show");
     }
 
+    /**
+     * unsets the classes for mobile view ("S view")
+     * when resizing from small to normal view
+     */
     function runL () {
+        /** removes click event for menu button if binded (case of resize S->L) **/
         $(".menu_closed").off("click", menuToggle);
 
         /** style class changes **/
@@ -60,7 +71,11 @@ $(document).ready (function menu_handler_js () {
             .removeClass("nbg");
     }
 
+    /**
+     * sets the classes for mobile view ("S view")
+     */
     function runS () {
+        /** removes click event to avoid multiple bindings **/
         $(".menu_closed").off("click", menuToggle);
 
         /** style class changes **/
@@ -90,6 +105,9 @@ $(document).ready (function menu_handler_js () {
             .not("nbg").addClass("nbg");
     }
 
+    /**
+     * binds the menu show-hide action
+     */
     function menuActionsS () {
         if (mode == 'S') {
             $(".menu_closed").on("click", menuToggle);
@@ -114,7 +132,7 @@ $(document).ready (function menu_handler_js () {
             console.log (mode);
             console.log ($(window).innerWidth ());
     }
-
+/* ------------------ WORKING SCRIPT -------------------- */
     setMode ();
     addLinkDecorators();
     menuActionsS();
