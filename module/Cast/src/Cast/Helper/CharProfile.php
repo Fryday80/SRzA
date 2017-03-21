110e965731d15c753809c26cc77f58c2ee4c4031
Fryday80 <Fry.LMU@gmx.de>
2017-03-21 17:28:03 +0100

    cleanup and css stuff

--- module/Cast/src/Cast/Helper/CharProfile.php
+++ module/Cast/src/Cast/Helper/CharProfile.php
@@ -6,7 +6,6 @@ use Auth\Model\AuthStorage;
 
 class CharProfile extends AbstractHelper
 {
-    private $data;
 
     public function __construct() {
         return $this;
@@ -16,13 +15,26 @@ class CharProfile extends AbstractHelper
         return $this;
     }
     public function small($char) {
-        return '<ul>'.
-            '   <li>Name: '.$char['name'].'</li>'.
-            '   <li>surename '.$char['surename'].'</li>'.
-            '   <li>Beruf: '.$char['job_name'].'</li>'.
-            '   <li>Vita: '.$char['vita'].'</li>'.
-            '   <li>&nbsp;</li>'.
-            '   <li>Banner image</li>'.
-            '</ul>';
+        $return = '<ul>'.
+                    '   <li>Name: '.$char['name'].'</li>'.
+                    '   <li>surename '.$char['surename'].'</li>'.
+                    '   <li>Beruf: '.$char['job_name'].'</li>'.
+                        '   <div class="banner_small_sign" ';
+        foreach ($char as $key => $value)
+        {
+            $return .= $this->addData($key, $value);
+        }
+        $return .=      '</div>'.
+                        '   <li>Vita: '.$char['vita'].'</li>'.
+                        '   <li>&nbsp;</li>'.
+                        '   <li>Banner image</li>'.
+                '</ul>';
+            return $return;
+    }
+    private function addData($key, $value)
+    {
+        if ($key == 'id')return 'id="'. $value .'" ';
+        if ($key == 'vita') return;
+        else return 'data-'.$key.'=" ' . $value . '" ';
     }
 }
