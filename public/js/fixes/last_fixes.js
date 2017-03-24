7b3c59f52237643902fd1446193666bb6d77db8d
Fryday80 <Fry.LMU@gmx.de>
2017-03-24 12:09:37 +0100

    removed unused code

--- public/js/fixes/last_fixes.js
+++ public/js/fixes/last_fixes.js
@@ -25,32 +25,6 @@ window.last_fixes = {
                 speed: 3
             });  */
         });
-
-        /**
-         * manages Content switching
-         */
-
-        $(".angebote .box").not(".switchable .box").css("max-width", "50%")
-            .css("margin", "1vw")
-            .css("box-shadow", "0.5vw 0.5vw 0.5vw grey");
-        $(".switchable .titel").addClass("titi")
-            .removeClass("titel");
-        $(".switchable .content").addClass("conti")
-            .removeClass("content");
-        $(".switchable")
-            .css("box-shadow", "0.5vw 0.5vw 0.5vw grey")
-            .css("margin", "1vw")
-            .css("width", "40%")
-            .css("float", "left");
-        $(".switchable .conti").toggle();
-        $(".switchable").on("click", function (e){
-            $(".conti", this)
-                .toggle("content");
-            $(".titi", this)
-                .toggle("titel");
-            console.log ("huhu");
-        });
-
     }
 }
 $(function() {
