af997dd6bcb77f767cc77b09ca61635e6b0fc114
salt <boluuuu@gmail.com>
2017-03-24 18:30:33 +0100

    db update

--- module/Album/src/Album/Service/GalleryService.php
+++ module/Album/src/Album/Service/GalleryService.php
@@ -27,8 +27,6 @@ Class GalleryService
         $this->mediaService = $sm->get('MediaService');
     }
 
-
-
     public function getAllAlbums() {
         $result = array();
         $fileName = '/album.conf';
@@ -40,156 +38,183 @@ Class GalleryService
         }
         return $result;
     }
-    public function getAlbum($path) {
-        $path = $this->galleryPath.'/'.$path;
+
+    public function getAlbum($name) {
+        $path = $this->galleryPath.'/'.$name;
         $fileName = '/album.conf';
         if ($this->mediaService->fileExists($path.$fileName) ) {
             $album = new AlbumModel($path, $this->mediaService);
             $album->loadImages();
             return $album;
+        } else {
+            dump("path not exists");
+            dumpd($path.$fileName);
         }
     }
 
+    public function getRandomImage($count = 1) {
+        //get random album
+        $galleryDirs = $this->getAllAlbums();
+        if (count($galleryDirs) == 0) return [];
+        $randomIndex = rand(0, count($galleryDirs) -1);
+        $album = $galleryDirs[$randomIndex];
+        $album->loadImages();
+        //if (!$album) return [];
+        $result = [];
 
+        for($i = 0; $i < $count; $i++) {
+            $item = $album->getRandomItem();
 
-
-
-
-
-
-
-
-
-
-    /* read ******************************** */
-    public function getAlbumByID($id) {
-        return $this->albumsTable->getById($id);
-    }
-
-    public function fetchWholeAlbumData($id) {
-        $albums = $this->albumsTable->getById($id);
-        $images = $this->imagesTable->getImagesByAlbumID($id);
-        return array($albums, $images);
-    }
-    public function getImageByID($id) {
-        return $this->imagesTable->getById($id);
-    }
-
-/* add ********************************* */
-    /**
-     * @param $data
-     * @return mixed $id on success or bool false on fail
-     */
-    public function addAlbum($data) {
-        return $this->albumsTable->add($data);
-    }
-
-    /**
-     * @param $data
-     * @return mixed $id on single image success, bool false on fail, bool true on image array add
-     */
-    public function addImage($data) {
-        if ($data['id']) {
-            return ($this->imagesTable->add($data));
-        }
-        if ($data[0]['id']){
-            foreach ($data as $image){
-                $this->imagesTable->add($image);
-            }
-            return true;
-        }
-        else {return false;}
-    }
-
-
-/* update ********************************* */
-    /**
-     * @param $data array of album information
-     * @return mixed $id on success or bool false on fail
-     */
-    public function updateAlbum($data) {
-        return $this->albumsTable->change($data['id'], $data);
-    }
-
-    /**
-     * Update Image or Images
-     * @param $data array with image data or array of images ([0]=> image data, [1] => image data
-     * 
-     * @return mixed $id on success or bool false on fail
-     */
-    public function updateImage($data) {
-        if ($data['id']) {
-            return ($this->imagesTable->change($data['id'], $data));
-        }
-        if ($data[0]['id']){
-            foreach ($data as $image){
-                $this->imagesTable->change($image['id'], $image);
-            }
-            return true;
-        }
-        else {return false;}
-    }
-
-/* delete ********************************* */
-    /**
-     * @param $id
-     * @return bool true on success
-     */
-    public function deleteWholeAlbum($id) {
-        $this->deleteAllAlbumImages($id);
-        $this->albumsTable->remove($id);
-        return true;
-    }
-
-    /**
-     * @param $image_id
-     * @return bool true on success or false on fail
-     */
-    public function deleteImage($image_id) {
-        $this->imagesTable->remove($image_id);
-        return $this->albumImagesTable->removeByImageID($image_id);
-    }
-
-    /**
-     * @param $id
-     * @return bool true on success or false on fail
-     */
-    public function deleteAllAlbumImages($id) {
-        $images = $this->imagesTable->getImagesByAlbumID($id);
-        $this->albumImagesTable->removeByAlbumID($id);
-        foreach ($images as $image){
-            $this->imagesTable->remove ($image['id']);
-        }
-        return true;
-    }
-    
-    public function storeAlbum ($album_data){
-
-        $album_data = $this->exchangeDataArray($album_data);
-
-        if ($album_data['id'] == '' ){
-            $this->addAlbum($album_data);
-        }
-        else {
-            $this->updateAlbum($album_data);
-        }
-    }
-
-    private function exchangeDataArray($data){
-        $int_array = array (
-            'id', 'timestamp', 'visibility', 'album_id', 'image_id');
-        $exclude_array = array (
-            'validators','submit','date');
-        $new_data = array();
-        foreach ($data as $key => $value){
-            if (in_array($key, $int_array)){
-                $value = (int) $value;
-            }
-
-            if (!in_array($key, $exclude_array)) {
-                $new_data[$key] = $value;
+            if (!$item) break;
+            if(!in_array($item, $result)) {
+                array_push($result, $item);
+            } else {
+                $i--;
             }
         }
-        return $new_data;
+        return $result;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+//    /* read ******************************** */
+//    public function getAlbumByID($id) {
+//        return $this->albumsTable->getById($id);
+//    }
+//
+//    public function fetchWholeAlbumData($id) {
+//        $albums = $this->albumsTable->getById($id);
+//        $images = $this->imagesTable->getImagesByAlbumID($id);
+//        return array($albums, $images);
+//    }
+//    public function getImageByID($id) {
+//        return $this->imagesTable->getById($id);
+//    }
+//
+///* add ********************************* */
+//    /**
+//     * @param $data
+//     * @return mixed $id on success or bool false on fail
+//     */
+//    public function addAlbum($data) {
+//        return $this->albumsTable->add($data);
+//    }
+//
+//    /**
+//     * @param $data
+//     * @return mixed $id on single image success, bool false on fail, bool true on image array add
+//     */
+//    public function addImage($data) {
+//        if ($data['id']) {
+//            return ($this->imagesTable->add($data));
+//        }
+//        if ($data[0]['id']){
+//            foreach ($data as $image){
+//                $this->imagesTable->add($image);
+//            }
+//            return true;
+//        }
+//        else {return false;}
+//    }
+//
+//
+///* update ********************************* */
+//    /**
+//     * @param $data array of album information
+//     * @return mixed $id on success or bool false on fail
+//     */
+//    public function updateAlbum($data) {
+//        return $this->albumsTable->change($data['id'], $data);
+//    }
+//
+//    /**
+//     * Update Image or Images
+//     * @param $data array with image data or array of images ([0]=> image data, [1] => image data
+//     *
+//     * @return mixed $id on success or bool false on fail
+//     */
+//    public function updateImage($data) {
+//        if ($data['id']) {
+//            return ($this->imagesTable->change($data['id'], $data));
+//        }
+//        if ($data[0]['id']){
+//            foreach ($data as $image){
+//                $this->imagesTable->change($image['id'], $image);
+//            }
+//            return true;
+//        }
+//        else {return false;}
+//    }
+//
+///* delete ********************************* */
+//    /**
+//     * @param $id
+//     * @return bool true on success
+//     */
+//    public function deleteWholeAlbum($id) {
+//        $this->deleteAllAlbumImages($id);
+//        $this->albumsTable->remove($id);
+//        return true;
+//    }
+//
+//    /**
+//     * @param $image_id
+//     * @return bool true on success or false on fail
+//     */
+//    public function deleteImage($image_id) {
+//        $this->imagesTable->remove($image_id);
+//        return $this->albumImagesTable->removeByImageID($image_id);
+//    }
+//
+//    /**
+//     * @param $id
+//     * @return bool true on success or false on fail
+//     */
+//    public function deleteAllAlbumImages($id) {
+//        $images = $this->imagesTable->getImagesByAlbumID($id);
+//        $this->albumImagesTable->removeByAlbumID($id);
+//        foreach ($images as $image){
+//            $this->imagesTable->remove ($image['id']);
+//        }
+//        return true;
+//    }
+//
+//    public function storeAlbum ($album_data){
+//
+//        $album_data = $this->exchangeDataArray($album_data);
+//
+//        if ($album_data['id'] == '' ){
+//            $this->addAlbum($album_data);
+//        }
+//        else {
+//            $this->updateAlbum($album_data);
+//        }
+//    }
+//
+//    private function exchangeDataArray($data){
+//        $int_array = array (
+//            'id', 'timestamp', 'visibility', 'album_id', 'image_id');
+//        $exclude_array = array (
+//            'validators','submit','date');
+//        $new_data = array();
+//        foreach ($data as $key => $value){
+//            if (in_array($key, $int_array)){
+//                $value = (int) $value;
+//            }
+//
+//            if (!in_array($key, $exclude_array)) {
+//                $new_data[$key] = $value;
+//            }
+//        }
+//        return $new_data;
+//    }
 }
--- module/Media/src/Media/Service/MediaService.php
+++ module/Media/src/Media/Service/MediaService.php
@@ -119,157 +119,157 @@ class MediaService {
 
 
 
-
-
-    function getImportPreview() {
-        $path = $this->uploadPath;
-        $files = $this->readUploadFolder($path);
-        $errors = [];
-        $result = [];
-
-        foreach ($files as $file) {
-            $realPath = realpath($file['fullPath']);
-            $targetPath = $this->dataPath.$file['path'].$file['name'];
-            array_push($result, new MediaItem($realPath));
-            /*
-            if ($file['type'] == 'folder') {
-                //check if folder exist in data folder
-                if (is_dir($targetPath)) {
-                    //exists allready -> go on recursive
-                    $this->import($realPath);
-                } else {
-                    //do not exists -> move hole folder
-                    rename($realPath, $targetPath);
-                }
-            } else {
-                if (!file_exists($targetPath)) {
-                    //move file to target
-                    rename($realPath, $targetPath);
-                } else {
-                    array_push($errors, array(
-                        'msg' => 'File exists allready',
-                        'file' =>$file
-                    ));
-                }
-            }*/
-        }
-        return $result;
-    }
-    function import($path = null) {
-        if ($path == null) $path = $this->uploadPath;
-        $files = $this->readUploadFolder($path);
-        $errors = [];
-        foreach ($files as $file) {
-            $realPath = realpath($file['fullPath']);
-            $targetPath = $this->dataPath.$file['path'].$file['name'];
-            if ($file['type'] == 'folder') {
-                //check if folder exist in data folder
-                if (is_dir($targetPath)) {
-                    //exists allready -> go on recursive
-                    $this->import($realPath);
-                } else {
-                    //do not exists -> move hole folder
-                    rename($realPath, $targetPath);
-                }
-            } else {
-                if (!file_exists($targetPath)) {
-                    //move file to target
-                    rename($realPath, $targetPath);
-                } else {
-                    array_push($errors, array(
-                        'msg' => 'File exists allready',
-                        'file' =>$file
-                    ));
-                }
-            }
-        }
-        return $errors;
-    }
-    function getAlbumFolderNames() {
-        $albumPath = realpath($this->dataPath.'/gallery/');
-        $dir = scandir($albumPath);
-        $result = array();
-        foreach ($dir as $key => $value) {
-            if ($value == '.' || $value == '..') continue;
-            if( is_dir ($albumPath.'/'.$value) ) {
-                array_push($result, $value);
-            }
-        }
-        return $result;
-    }
-    /**
-     * @param {string} $albumName or better the folder name where the images life in
-     * @return {array} array with all images and there paths'
-     */
-    function getAlbumFiles($albumName) {
-        $albumPath = realpath($this->dataPath.'/gallery/'.$albumName);
-        if (is_dir($albumPath) ) {
-            //Cast folder exists -> read all containing files
-            $dir = scandir($albumPath);
-            $result = array();
-            foreach ($dir as $key => $value) {
-                if ($value == '.' || $value == '..') continue;
-                $relPath = str_replace($this->dataPath, '', $albumPath);
-                $relPath = str_replace("\\", "/", $relPath);
-                $fileInfo = pathinfo($relPath.'/'.$value);
-                if ($relPath == '') $relPath = '/';
-                $type = (is_dir($albumPath.'\\'.$value))? 'folder': 'file';
-                $fileInfo['url'] = '/media/image'.$relPath.'/'.$value;
-                $fileInfo['fullPath'] = $albumPath.'\\'.$value;
-
-                array_push($result, $fileInfo);
-            }
-            return $result;
-        }
-        //check if Cast exists
-        //read files
-        //return files
-        return array();
-    }
-
-    /**
-     * @param $path absolute path to folder
-     * @return array array with the folder content
-     */
-    private function readUploadFolder($path) {
-        $dir = scandir($path);
-        $result = array();
-        foreach ($dir as $key => $value) {
-            if ($value == '.' || $value == '..') continue;
-            $relPath = str_replace($this->uploadPath, '', $path);
-            if ($relPath == '') $relPath = '/';
-            $type = (is_dir($path.'\\'.$value))? 'folder': 'file';
-            array_push($result, array(
-                'name' => $value,
-                'path' => $relPath,
-                'fullPath' => $path.'\\'.$value,
-                'type' => $type
-            ));
-        }
-        return $result;
-    }
-    /**
-     * returns a multidimensional array with folders
-     */
-    function getAllFolders() {
-        return array(
-            'name',
-            'path',
-            'folders' => array(
-                'name',
-                'path',
-                'folders' => array())
-        );
-    }
-    /**
-     * @param {string} $path relative path to the data folder
-     */
-    function getFolder($path) {
-        //path exists
-        //load folder.perm
-        //check permission   permission template = recource is media  privileg is /path/to/folder
-        //if no permission ?? redirect to login ?? or return false
-        //return array with file paths'
-    }
+//
+//
+//    function getImportPreview() {
+//        $path = $this->uploadPath;
+//        $files = $this->readUploadFolder($path);
+//        $errors = [];
+//        $result = [];
+//
+//        foreach ($files as $file) {
+//            $realPath = realpath($file['fullPath']);
+//            $targetPath = $this->dataPath.$file['path'].$file['name'];
+//            array_push($result, new MediaItem($realPath));
+//            /*
+//            if ($file['type'] == 'folder') {
+//                //check if folder exist in data folder
+//                if (is_dir($targetPath)) {
+//                    //exists allready -> go on recursive
+//                    $this->import($realPath);
+//                } else {
+//                    //do not exists -> move hole folder
+//                    rename($realPath, $targetPath);
+//                }
+//            } else {
+//                if (!file_exists($targetPath)) {
+//                    //move file to target
+//                    rename($realPath, $targetPath);
+//                } else {
+//                    array_push($errors, array(
+//                        'msg' => 'File exists allready',
+//                        'file' =>$file
+//                    ));
+//                }
+//            }*/
+//        }
+//        return $result;
+//    }
+//    function import($path = null) {
+//        if ($path == null) $path = $this->uploadPath;
+//        $files = $this->readUploadFolder($path);
+//        $errors = [];
+//        foreach ($files as $file) {
+//            $realPath = realpath($file['fullPath']);
+//            $targetPath = $this->dataPath.$file['path'].$file['name'];
+//            if ($file['type'] == 'folder') {
+//                //check if folder exist in data folder
+//                if (is_dir($targetPath)) {
+//                    //exists allready -> go on recursive
+//                    $this->import($realPath);
+//                } else {
+//                    //do not exists -> move hole folder
+//                    rename($realPath, $targetPath);
+//                }
+//            } else {
+//                if (!file_exists($targetPath)) {
+//                    //move file to target
+//                    rename($realPath, $targetPath);
+//                } else {
+//                    array_push($errors, array(
+//                        'msg' => 'File exists allready',
+//                        'file' =>$file
+//                    ));
+//                }
+//            }
+//        }
+//        return $errors;
+//    }
+//    function getAlbumFolderNames() {
+//        $albumPath = realpath($this->dataPath.'/gallery/');
+//        $dir = scandir($albumPath);
+//        $result = array();
+//        foreach ($dir as $key => $value) {
+//            if ($value == '.' || $value == '..') continue;
+//            if( is_dir ($albumPath.'/'.$value) ) {
+//                array_push($result, $value);
+//            }
+//        }
+//        return $result;
+//    }
+//    /**
+//     * @param {string} $albumName or better the folder name where the images life in
+//     * @return {array} array with all images and there paths'
+//     */
+//    function getAlbumFiles($albumName) {
+//        $albumPath = realpath($this->dataPath.'/gallery/'.$albumName);
+//        if (is_dir($albumPath) ) {
+//            //Cast folder exists -> read all containing files
+//            $dir = scandir($albumPath);
+//            $result = array();
+//            foreach ($dir as $key => $value) {
+//                if ($value == '.' || $value == '..') continue;
+//                $relPath = str_replace($this->dataPath, '', $albumPath);
+//                $relPath = str_replace("\\", "/", $relPath);
+//                $fileInfo = pathinfo($relPath.'/'.$value);
+//                if ($relPath == '') $relPath = '/';
+//                $type = (is_dir($albumPath.'\\'.$value))? 'folder': 'file';
+//                $fileInfo['url'] = '/media/image'.$relPath.'/'.$value;
+//                $fileInfo['fullPath'] = $albumPath.'\\'.$value;
+//
+//                array_push($result, $fileInfo);
+//            }
+//            return $result;
+//        }
+//        //check if Cast exists
+//        //read files
+//        //return files
+//        return array();
+//    }
+//
+//    /**
+//     * @param $path absolute path to folder
+//     * @return array array with the folder content
+//     */
+//    private function readUploadFolder($path) {
+//        $dir = scandir($path);
+//        $result = array();
+//        foreach ($dir as $key => $value) {
+//            if ($value == '.' || $value == '..') continue;
+//            $relPath = str_replace($this->uploadPath, '', $path);
+//            if ($relPath == '') $relPath = '/';
+//            $type = (is_dir($path.'\\'.$value))? 'folder': 'file';
+//            array_push($result, array(
+//                'name' => $value,
+//                'path' => $relPath,
+//                'fullPath' => $path.'\\'.$value,
+//                'type' => $type
+//            ));
+//        }
+//        return $result;
+//    }
+//    /**
+//     * returns a multidimensional array with folders
+//     */
+//    function getAllFolders() {
+//        return array(
+//            'name',
+//            'path',
+//            'folders' => array(
+//                'name',
+//                'path',
+//                'folders' => array())
+//        );
+//    }
+//    /**
+//     * @param {string} $path relative path to the data folder
+//     */
+//    function getFolder($path) {
+//        //path exists
+//        //load folder.perm
+//        //check permission   permission template = recource is media  privileg is /path/to/folder
+//        //if no permission ?? redirect to login ?? or return false
+//        //return array with file paths'
+//    }
 
 }
