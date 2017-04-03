7f5a0a1896b429cfcc32dc2fe897b721e053f642
Fryday80 <Fry.LMU@gmx.de>
2017-04-03 15:37:07 +0200

    Log in box
    -closing (inactivating) animation added

--- public/js/globalUsage/logOutDesigner/logOutDesigner.js
+++ public/js/globalUsage/logOutDesigner/logOutDesigner.js
@@ -3,10 +3,18 @@ $(document).ready(function () {
     // var parent = $("div.rightbarup");
         
     function loggingMover(){
-
+        // temporary styling will be out sourced to css
         $(".log_me_out").appendTo("body")
             .css("position", "absolute");
         $("box.login").appendTo("body");
+
+        // real desingner function
+        $("box.login").on("click", function(){
+            $("box.login").toggleClass("login-active");
+        });
+        $("box.login").on("mouseout", function(){
+            $("box.login").not(".login-inactive").addClass("login-inactive");
+        });
     }
 
     if( !(window.matchMedia('(max-width: 700px)').matches) )
