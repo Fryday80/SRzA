dcb30f070013a6e9a349490b72932067957f85ea
Fryday80 <Fry.LMU@gmx.de>
2017-04-07 12:15:47 +0200

    js
    <popUp>
    
    expanded popUp.js from disclaimer -> disclaimer + impressum

--- public/js/globalUsage/popUp/popUp.js
+++ public/js/globalUsage/popUp/popUp.js
@@ -3,14 +3,31 @@ $(document).ready(function () {
 
 //    sessionStorage.clear(); // for testing reasons
 
-    function disclaimerPop () {
-        var ele;
+    function disclaimerPop (popup) {
+        var ele,
+            popupType,
+            text,
+            boxtitel,
+            usedClass;
+
+        if (popup == 'disclaimer') {
+            text = "/disclaimer.txt";
+            usedClass = popup;
+            boxtitel = "Disclaimer";
+            popupType = "ok-deny";
+        }
+        if (popup == 'impressum') {
+            text = "/impressum.txt";
+            usedClass = popup;
+            boxtitel = "Impressum";
+            popupType = "ok";
+        }
 
         var markup = $.ajax({
-            url: "/disclaimer.txt",    // text for disclaimer
+            url: text,    // text for disclaimer
             async: true,
             success: function(e) {
-                console.log('disclaimer pop success');
+                // console.log('disclaimer pop success');
             },
             error: function(err) {
                 console.log(err);
@@ -20,29 +37,54 @@ $(document).ready(function () {
             $(ele).html(e);
         });
 
-        ele = $( "<div class='disclaimer'></div>" ).dialog({
-            modal: true,
-            title: "Disclaimer",
-            height: "auto",
-            width: "auto",
-            open: function () {
-                $(".ui-dialog-titlebar-close").hide();      //removes  X in corner
-            },
-            buttons: {
-                ok: function () {
-                    sessionStorage.setItem('isshow', 1);
-                    $(this).dialog("close");
-                }
-                ,
-                deny: function () {
-                    var url = "http://www.google.de";
-                    window.location = url;
-                }
-            }
-         });
+        if (popupType == "ok-deny"){
+            ele = $( "<div class='"+usedClass+"'></div>" )
+                .dialog({
+                    modal: true,
+                    title: boxtitel,
+                    height: "auto",
+                    width: "auto",
+                    open: function () {
+                        $(".ui-dialog-titlebar-close").hide();      //removes  X in corner
+                    },
+                    buttons: {
+                        ok: function () {
+                            sessionStorage.setItem('isshow', 1);
+                            $(this).dialog("close");
+                        }
+                        ,
+                            deny: function () {
+                                var url = "http://www.google.de";
+                                window.location = url;
+                            }
+                    }
+                });
+        }
+        if (popupType == "ok") {
+            ele = $( "<div class='"+usedClass+"'></div>" )
+                .dialog({
+                    modal: true,
+                    title: boxtitel,
+                    height: "auto",
+                    width: "auto",
+                    open: function () {
+                        $(".ui-dialog-titlebar-close").hide();      //removes  X in corner
+                    },
+                    buttons: {
+                        ok: function () {
+                            $(this).dialog("close");
+                        }
+                    }
+                });
+        }
     }
     if(sessionStorage && !sessionStorage.getItem('isshow')){
         disclaimerPop();
     }
-    $(".disclaim").on("click", disclaimerPop);
+    $(".disclaim").on("click", function () {
+        disclaimerPop("disclaimer");
+    });
+    $(".impressum").on("click", function () {
+        disclaimerPop("impressum");
+    });
 });
