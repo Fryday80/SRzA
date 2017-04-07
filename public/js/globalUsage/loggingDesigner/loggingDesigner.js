0dd73b24155aaed2befdefaef79aa0bde9fd479b
Fryday80 <Fry.LMU@gmx.de>
2017-04-07 14:05:31 +0200

    js, CSS/Less
    restyled loggingDesigner.js
    split functions up, added "state" vars
    cleaner code now

--- public/js/globalUsage/loggingDesigner/loggingDesigner.js
+++ public/js/globalUsage/loggingDesigner/loggingDesigner.js
@@ -1,39 +1,85 @@
 
 $(document).ready(function () {
-    var loginmodified = false;
-        
-    function loggingMover(){
-        if( !(window.matchMedia('(max-width: 700px)').matches) ) {
-            // temporary styling will be out sourced to css
-            $(".logout").appendTo("body")
-                .css("position", "absolute");
-            $(".rightbarDown box.login").appendTo("body")
-                .addClass("topBox");
+    var loggingFunctionSet = false,
+        htmlModified = false;
 
-            // real desingner function
-            $("box.login.topBox").off("click");
+    /**
+     * append css actions and functionality (css slide actions, stay open on click)
+     * to login box
+     * dependent on state var "loggingFunctionSet"
+     * and changes it's state
+     */
+    function loggingFunction(){
+        if (!loggingFunctionSet) {
             $("box.login.topBox").on("click", function () {
                 $("box.login.topBox").toggleClass("login-active");
             });
             $("box.login.topBox").on("mouseout", function () {
                 $("box.login.topBox").not(".login-inactive").addClass("login-inactive");
             });
-            loginmodified = true;
+            loggingFunctionSet = true;
         }
     }
 
-    function setBack (){
-        if( (window.matchMedia('(max-width: 700px)').matches)) {
+    /**
+     * remove css actions and functionality (css slide actions, stay open on click)
+     * from login box
+     * dependent on state var "loggingFunctionSet"
+     * and changes it's state
+     */
+    function resetLoggingFunction (){
+        if(loggingFunctionSet) {
+            $("box.login.topBox").off("click");
+            loggingFunctionSet = false;
+        }
+    }
+
+    /**
+     * change the HTML structure for browser view
+     * dependent on state var "htmlModified"
+     * and changes it's state
+     */
+    function setBrowserHTML(){
+        if (!htmlModified) {
+            $(".logout").appendTo("body");
+            $(".rightbarDown box.login").appendTo("body")
+                .addClass("topBox");
+            htmlModified = true;
+        }
+    }
+
+    /**
+     * change the HTML structure for mobile view
+     * dependent on state var "htmlModified"
+     * and changes it's state
+     */
+    function setMobileHTML (){
+        if (htmlModified) {
             $(".logging").appendTo(".rightbarDown")
-                .removeAttr("style")
                 .removeClass("login-active")
                 .removeClass("login-inactive");
+            htmlModified = false;
         }
     }
 
-    loggingMover();
+    /**
+     * runs the functions dependant from view size => mobile or browser view
+     */
+    function run (){
+        if( (window.matchMedia('(max-width: 700px)').matches))
+        {
+            setMobileHTML();
+            resetLoggingFunction();
+        }
+        else
+        {
+            setBrowserHTML();
+            loggingFunction();
+        }
+    }
+
+    run();
     $(window).resize(function(){
-        loggingMover();
-        setBack();
+        run();
     });
 });
