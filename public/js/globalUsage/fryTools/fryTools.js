3164567c0a237a0418b4645cdc3bb538aa2733e7
Fryday80 <Fry.LMU@gmx.de>
2017-04-08 15:33:29 +0200

    jgallery patch finshed
    js tools (fryTools) implemented

---
+++ public/js/globalUsage/fryTools/fryTools.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Fry on 08.04.2017.
+ */
+var debug = true;
+
+function deBugger(variable){
+    if(debug) {
+        console.log(variable);
+    }
+}
--- public/js/selectedUsage/jgallery-patch/jgallery-patch.js
+++ public/js/selectedUsage/jgallery-patch/jgallery-patch.js
@@ -2,15 +2,31 @@
  * Created by Fry on 08.04.2017.
  */
 $(document).ready(function(){
+    /** debug info **/
+   deBugger('File: jgallery-patch.js');
 
-    function testMode(){
-        console.log('yeehaw');
-    }
+    /** waiting for the DOM **/
+    setTimeout(function(){
 
-
-    $( "span.fa.change-mode" )
-        .on("click", function(){
-            testMode();
-        });
-    console.log($( "jgallery-full-screen" ));
+        function fixZIndex(){
+            deBugger('func fixZIndex triggered');
+            if ($("span.fa.change-mode").hasClass('fa-compress')) {
+                $('#content').css("z-index",2000);
+                $('.logout').hide();
+                deBugger('fullscreen detected')
+            }
+            else {
+                $('#content').removeAttr("style");
+                $('.logout').show();
+            }
+        }
+        
+        $( "span.fa.change-mode" )      //das ist das element mim icon für fullscreen $( ".change-mode" ) müsste sogar reichen
+            .on("click", function(){
+                deBugger('click on $( "span.fa.change-mode" ) triggered');
+                fixZIndex();
+            });
+        deBugger($( "span.fa.change-mode" ));
+        deBugger("jgallery-patch loaded");
+    },10);
 });
