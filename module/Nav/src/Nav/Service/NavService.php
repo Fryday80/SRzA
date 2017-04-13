5481e3aeb1f5ed9c52b78b942c55f257663f00f3
Fryday80 <Fry.LMU@gmx.de>
2017-04-13 13:11:01 +0200

    huren scheiß fuck teil 6 -> service prepared

---
+++ module/Nav/src/Nav/Service/NavService.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fry
+ * Date: 13.04.2017
+ * Time: 13:01
+ */
+
+namespace Nav\Service;
+
+
+use Auth\Model\PermissionTable;
+use Auth\Model\ResourceTable;
+use Auth\Model\RolePermissionTable;
+use Auth\Model\RoleTable;
+
+class NavService
+{
+    /** @var  $roleTable RoleTable */
+    /** @var $permissionTable PermissionTable */
+    /** @var $resourceTable ResourceTable */
+    /** @var $rolePermissionTable RolePermissionTable */
+    private $roleTable;
+    private $permissionTable;
+    private $resourceTable;
+    private $rolePermissionTable;
+
+
+    function __construct($sm)
+    {
+        $this->roleTable = $sm->get('Auth\Model\RoleTable');
+        $this->permissionTable = $sm->get('Auth\Model\PermissionTable');
+        $this->resourceTable = $sm->get('Auth\Model\ResourceTable');
+        $this->rolePermissionTable = $sm->get('Auth\Model\RolePermissionTable');
+    }
+}
