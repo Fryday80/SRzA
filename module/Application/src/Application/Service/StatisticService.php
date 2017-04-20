1ffd8d11ec51501b9a7b2f92b7f0675b93ce73ed
Fryday80 <Fry.LMU@gmx.de>
2017-04-20 21:01:04 +0200

    NEW STATS 1.1 Page Hit & Action Log working

--- module/Application/src/Application/Service/StatisticService.php
+++ module/Application/src/Application/Service/StatisticService.php
@@ -34,7 +34,6 @@ class StatisticService
         /**** STORAGE ****/
         $this->storagePath = getcwd().STORAGE_PATH;
         $this->collection = (file_exists($this->storagePath)) ? $this->loadFile() : new StatisticDataCollection($sm);
-
     }
 
     /**** EVENTS ****/
@@ -42,39 +41,37 @@ class StatisticService
     {
         /** @var  $a AccessService*/
         $a = $this->sm->get('AccessService');
-        $userId = ($a->getUserID() == "-1")? 0 : (int)$a->getUserID();
+        $serverPHPData = $e->getApplication()->getRequest()->getServer()->toArray();
+        $now = time();
+        $userId = $a->getUserID();
+        $userId = ($userId == "-1")? 0 : (int)$userId;
         $userName = $a->getUserName();
         $userName = ($userName == "") ? "Guest" : $userName;
-        $this->updateActiveUserData($userId, $userName);
-        $serverPHPData = $e->getApplication()->getRequest()->getServer()->toArray();
-        $ajax = $e->getApplication()->getRequest()->isXmlHttpRequest();
-        if($ajax) return ; //@todo check if its in blacklist
-        $now = time();
+        $sid = $a->session->getManager()->getId();
+        $url = $activeUserData['last_action_url'] = ($serverPHPData['REQUEST_URI'] == '/') ? '/Home' : $serverPHPData['REQUEST_URI'];
+        if( $this->ajaxFilter( $e->getApplication()->getRequest()->isXmlHttpRequest(), $url ) ) return ; //@todo
         $replace = array( "http://", $serverPHPData['HTTP_HOST'] );
         $referrer = (isset ($serverPHPData['HTTP_REFERER']) ) ? $serverPHPData['HTTP_REFERER'] : "direct call";
-//        $relativeReferrerURL = str_replace( $replace,"", $referrer, $counter );
-//        $redirect = (isset ($serverPHPData['REDIRECT_STATUS']))? $serverPHPData['REDIRECT_STATUS'] : "no redirect"; //set if redirected
-//        $redirectedTo = (isset ($serverPHPData['REDIRECT_URL']) ) ? $serverPHPData['REDIRECT_URL'] : "no redirect";
+        $relativeReferrerURL = str_replace( $replace,"", $referrer, $counter );
+        $redirect = (isset ($serverPHPData['REDIRECT_STATUS']))? $serverPHPData['REDIRECT_STATUS'] : "no redirect"; //set if redirected
+        $redirectedTo = (isset ($serverPHPData['REDIRECT_URL']) ) ? $serverPHPData['REDIRECT_URL'] : "no redirect";
         
         // active users data
         $activeUserData['time'] = $now;
-        $activeUserData['ip'] = $e->getApplication()->getRequest()->getServer('REMOTE_ADDR');
-        $activeUserData['sid'] = $a->session->getManager()->getId();
+        $activeUserData['ip'] = $serverPHPData['REMOTE_ADDR'];
+        $activeUserData['sid'] = $sid;
         $activeUserData['userId'] = $userId;
         $activeUserData['userName'] = $userName;
         $activeUserData['data'] = array();
-        $lastUrl = $activeUserData['last_action_url'] = ($serverPHPData['REQUEST_URI'] == '/') ? '/Home' : $serverPHPData['REQUEST_URI'];
 
         $activeUserData['data']['serverData'] = $serverPHPData;
-        
-        $this->updateActionsLog('site call', $lastUrl, 'regular call', $activeUserData);
-        $this->updatePageHit( $lastUrl);
-        $this->updateActiveUsers($activeUserData['sid'], $activeUserData['ip'], $lastUrl, $activeUserData);
-        // for testing:
-        if (( 5 > $this->getNumberOfLogs() ))
-            $this->updateSystemLog('DUMMY', 'dummy', 'dummy data');
+        $this->triggerOnDispatch( $now, $url, $userId, 'Type:onDispatch', 'Title', $activeUserData );
 
-
+    }
+    public function onError(MvcEvent $e) {
+        /** @var \Exception $exception */
+        $exception = $e->getResult()->exception;
+        $this->updateSystemLog("ROUTING", $exception->getMessage(), $e->getApplication()->getRequest()->getServer('REMOTE_ADDR'));
         $this->saveFile($this->collection);
     }
     public function onFinish()
@@ -82,36 +79,46 @@ class StatisticService
 //        $this->cache->setCache($this::ACTIONS_CACHE_NAME, $this->actionsLog);
     }
     /**** METHODS ****/
+    /**** TRIGGER SETS ****/
+    public function triggerOnDispatch( $now, $url, $type, $title, $userId, $actionsLogData = null, $activeUserData = null, $pageHitData = null )
+    {
+        $this->updateActionsLog($url, $now, $userId, $type, $title, 'regular call', $actionsLogData);
+        $this->updatePageHit( $url, $now, $userId, $pageHitData);
+        $this->updateActiveUsers($activeUserData['sid'], $activeUserData['ip'], $url, $activeUserData);
+
+        $this->saveFile($this->collection);
+    }
+
     /**** SET ****/
     /**** ACTIVE USERS ****/
-    public function updateActiveUsers($sid, $ip, $lastActionUrl, $data){
+    private function updateActiveUsers($sid, $ip, $lastActionUrl, $data){
         $this->collection->updateActiveUsers($sid, $ip, $lastActionUrl, $data);
+    }
+    /**** ACTIONS LOG ****/
+    private function updateActionsLog($url, $time, $userId, $type, $title, $msg, $data = null){
+        $this->collection->updateActionsLog($url, $time, $userId, $type, $title, $msg, $data);
+    }
+    /**** PAGE HITS ****/
+    private function updatePageHit($url, $time, $userId, $data = null){
+        $this->collection->updatePageHit($url, $time, $userId, $data);
+    }
+    /**** SYS LOG ****/
+    private function updateSystemLog($type, $msg, $url, $data = null){
+        $this->updateActionsLog('Error', $url, $msg, $data);
+        $this->collection->updateSystemLog($type, $msg, $data);
+    }
+
+
+    /**** GET ****/
+    /**** ACTIVE USERS ****/
+    public function getActiveUsers(){
+        return $this->collection->getActiveUsers();
     }
     public function getActiveGuests(){
         return $this->collection->getActiveGuests();
     }
     public function getGuestCount(){
         return $this->collection->getGuestCount();
-    }
-    /**** ACTIONS LOG ****/
-    public function updateActionsLog($type, $title, $msg, $data = null){
-        $this->collection->updateActionsLog($type, $title, $msg, $data);
-    }
-    /**** PAGE HITS ****/
-    public function updatePageHit($url, $data = null){
-        $this->collection->updatePageHit($url, $data);
-    }
-    /**** SYS LOG ****/
-    public function updateSystemLog($type, $msg, $data){
-        $this->collection->updateSystemLog($type, $msg, $data);
-    }
-    public function getNumberOfLogs(){
-        return $this->collection->getNumberOfLogs();
-    }
-    /**** GET ****/
-    /**** ACTIVE USERS ****/
-    public function getActiveUsers(){
-        return $this->collection->getActiveUsers();
     }
     /**** ACTIONS LOG ****/
     public function getActionsLog($since = null){
@@ -147,11 +154,10 @@ class StatisticService
         return $this->collection->getSystemLogByUser ($userId, $since);
     }
 
-    /**** PRIVATE HELPER ****/
-    private function updateActiveUserData($userId,$userName){
-        $this->collection->setUserId ($userId);
-        $this->collection->setUserName ($userName);
+    public function getNumberOfLogs(){
+        return $this->collection->getNumberOfLogs();
     }
+
     /**** DATA COLLECTION SAVE & RESTORE ****/
 
     private function saveFile($content) {
@@ -163,5 +169,15 @@ class StatisticService
         $content = file_get_contents($this->storagePath);
         $content = unserialize($content);
         return $content;
+    }
+
+    private function ajaxFilter($ajax, $url)
+    {
+        if ($ajax) {
+            // @todo whitelist
+            $whitelist = array('/');
+            if (in_array($url, $whitelist)) return true;
+        }
+        return false;
     }
 }
