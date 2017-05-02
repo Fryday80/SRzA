9f97cd67b30bfe22036049ebdec2df21707332d2
Fryday80 <Fry.LMU@gmx.de>
2017-05-02 18:37:24 +0200

    new blazon Helper

--- module/Cast/src/Cast/View/BlazonHelper.php
+++ module/Cast/src/Cast/View/BlazonHelper.php
@@ -12,11 +12,15 @@ class BlazonHelper extends AbstractHelper
     private $sm; // if sth is needed
     // $blazons needs to be stored in db
     private $blazons = array(
-                            0 => array ( 'url' => '/img/fry1.png' ),
-                            1 => array ( 'url' => '/img/default.png'),
+                            0 => array ( 'url' => '/img/blazons/shield-tross.big.png' ),
+                            1 => array ( 'url' => '/img/blazons/zuLeym.png' ),
+                            2 => array ( 'url' => '/img/blazons/fryschild.png'),
         );
     // $jobs needs to be stored in db and created automatically
-    private $jobs = array ('soldat' => '/img/swords.png'); // job strings
+    private $jobs = array ('soldat' => '/img/blazons/swords.png'); // job strings
+
+    // options
+    private $imgHeight = 200;
 
     function __construct($sm)
     {
@@ -31,6 +35,10 @@ class BlazonHelper extends AbstractHelper
             if ($baseId !== 0) {
                 bdump( $dumpMsg . ' of $baseId');
                 $baseId = 0;
+            }
+            if ($overlay1 !== null) {
+                bdump( $dumpMsg . ' of $overlay1');
+                $overlay1 = 'soldat';
             }
             if ($overlay2 !== null) {
                 bdump( $dumpMsg . ' of $overlay2');
@@ -55,69 +63,30 @@ class BlazonHelper extends AbstractHelper
 
     private function createBlazon($baseId, $job, $familyId, $class = null)
     {
-        if ($class !== null){
-            $class = "class='$class'";
-        } else {
-            $class = '';
-        }
-        $imgPath = '/temp/'. $baseId.$job.$familyId .'.png';
-        // check if combination exists
-        if (file_exists($imgPath)) return '<img ' . $class . 'src="' . $imgPath . '" >';
-        // validate
+        $class = ($class == null) ? 'x'.$job.$familyId : $baseId.$job.$familyId . ' ' . $class;
+
         $baseId = $this->validateIds($baseId);
         $job    = $this->validateJob($job);
         $familyId = $this->validateIds($familyId);
-
-        return $this->createOverlayOutput($baseId,$job, $familyId, $class, $imgPath);
+        $img = $this->createImg($baseId, $job, $familyId);
+        return '<div class="blazon '. $class .  '" style = " position: relative; height: '. $this->imgHeight .'px;">' . $img. '</div>';
 
 
     }
 
-    private function createOverlayOutput($baseId, $job, $familyId, $class, $imgPath)
+    private function createImg($baseId, $job, $familyId)
     {
-        $imgPre = '<img ' . $class . 'src="';
-        $imgPost = '" >';
-        $newheight = 0;
-        $newwidth = 0;
-        $newPic = '/public' . $imgPath;
-        $path = getcwd();
-        $pathPub = $path . '/public';
-
-        $blazonBaseUrl = $blazonOverlay1Url = $blazonOverlay2Url = false;
-        // case: family blazon
-        if ($job == 0 && $familyId == 0) return $imgPre . $this->blazons[$baseId]['url'] . $imgPost;
-        // case: modified blazon
-        $blazonBaseUrl = $pathPub . $this->blazons[$baseId]['url'];
-        if ($job !== 0)
-        {
-            $blazonOverlay1Url = $pathPub . $this->blazons[$familyId]['url'];
+        $backgroundImage2    = $backgroundImage3 = '';
+        // base blazon
+//        $scale = $this->isHigh($this->blazons[$baseId]['url']);
+        $backgroundImage1 = "<img src = '" . $this->blazons[$baseId]['url'] . "' style=' position: absolute; left: auto; right: auto; z-index: 3; height: " . $this->imgHeight . "px;'>";
+        if ( isset($this->jobs[$job]) ){
+            $backgroundImage2 = "<img src = '".  $this->jobs[$job] . "' style=' position: absolute; z-index: 5; height: 200px;'>";
         }
-        if ($familyId !== 0)
-        {
-            $blazonOverlay2Url = $pathPub . $this->blazons[$familyId]['url'];
+        if ( isset($this->blazons[$familyId]['url']) && $baseId !== $familyId){
+            $backgroundImage3 =  "<img src = '" . $this->blazons[$familyId]['url'] . "' style=' position: absolute; bottom: 0; left: 75px; z-index: 7; height: 90px;'>";
         }
-
-        // todo return Blazon = $baseId + family overlay = $family
-        $blazonBase = imagecreatefrompng($blazonBaseUrl);
-        list($baseWidth, $baseHeight) = getimagesize($blazonBaseUrl);
-        if ($blazonOverlay1Url) {
-            $blazonOverlay1 = imagecreatefrompng($blazonOverlay1Url);
-            list($over1Width, $over1Height) = getimagesize($blazonOverlay1Url);
-        }
-        if ($blazonOverlay2Url) {
-            $blazonOverlay2 = imagecreatefrompng($blazonOverlay2Url);
-            list($over2Width, $over2Height) = getimagesize($blazonOverlay2Url);
-        }
-        // cleanFix untill size is given
-        $newheight = $baseHeight;
-        $newwidth = $baseWidth;
-
-        $out = imagecreatetruecolor($newwidth, $newheight);
-        if ($blazonOverlay1Url) imagecopyresampled($out, $blazonBase, 0, 0, 0, 0, $newwidth, $newheight, $over1Width, $over1Width);
-        if ($blazonOverlay2Url) imagecopyresampled($out, $blazonOverlay2, 0, 0, 0, 0, ($newwidth/2), ($newheight/2), $over2Width, $over2Height);
-        imagepng($out, $path . $newPic, 5);
-        return $imgPre . $imgPath . $imgPost;
-
+        return $backgroundImage1.$backgroundImage2.$backgroundImage3;
     }
 
     private function validateIds($checkId)
@@ -131,5 +100,12 @@ class BlazonHelper extends AbstractHelper
         if ( $job == 0 ) return 0;
         if (! in_array( $job , $this->jobs ) ) return 0;
         return $job;
+    }
+
+    private function isHigh($url)
+    {
+        list($width, $height) = getimagesize($url);
+        if ($height > $width) return true;
+        return false;
     }
 }
