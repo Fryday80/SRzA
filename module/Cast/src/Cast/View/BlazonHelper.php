47713882ddb01a9ed274d50ccba4c21fb871585f
Fryday80 <Fry.LMU@gmx.de>
2017-05-03 02:01:27 +0200

    BlazonHelper commented and changed usage of the Helper

--- module/Cast/src/Cast/View/BlazonHelper.php
+++ module/Cast/src/Cast/View/BlazonHelper.php
@@ -5,79 +5,80 @@ namespace Cast\View;
 
 use Zend\View\Helper\AbstractHelper;
 
-const TEST_BLAZON = false; // cleanFix for testing
-
 class BlazonHelper extends AbstractHelper
 {
     // if sth is needed
     private $sm;
+    // data
+    private $blazons;
+    private $jobs;
+    private $preSetFactors;
 
-    // $blazons needs to be stored in db
-    private $blazons = array(
-                            0 => array ( 'url' => '/img/blazons/shield-tross.big.png' ),
-                            1 => array ( 'url' => '/img/blazons/zuLeym.png' ),
-                            2 => array ( 'url' => '/img/blazons/fryschild.png'),
-    );
-
-    // $jobs needs to be stored in db and created automatically
-    private $jobs = array (
-                        'soldat' => '/img/blazons/swords.png'
-    ); // job strings
-
-    private $options;
     private $activeOption;
 
     function __construct($sm)
     {
         $this->sm = $sm;
-        $this->standards();
-        $this->activeOption = $this->options['small'];
+        $this->setStandards();
+        $this->setBlazonsFromDB();
+        $this->setJobsFromDB();
+        $this->customizedSize( $this->preSetFactors['small'] );
     }
 
+    /** sets blazons size as factor of 1px       <br>
+     * or sets size to small if keyword does not fit
+     * @param mixed $size int|float  or keyword
+     * @return bool returns true       <br>
+     * or returns false if keyword does not fit
+     */
     public function setSize($size)
     {
-        if( is_int($size) || is_float($size) ){
+        if( is_int($size) || is_float($size) )
+        {
             $this->customizedSize($size);
-        } else {
-            if (key_exists($size, $this->options)) {
-                $this->activeOption = $this->options[$size];
-            } else {
-                $this->activeOption = $this->options['small'];
-            }
+            return true;
         }
+        if ( key_exists($size, $this->preSetFactors) )
+        {
+            $this->customizedSize( $this->preSetFactors[$size] );
+            return true;
+        }
+        $this->customizedSize( $this->preSetFactors['small'] );
+        return false;
     }
 
-    public function blazon($baseId, $overlay1 = null, $overlay2 = null, $class = null)
+    /** returns HTML string for blazon
+     * @param int $baseBlazon basic blazon ID
+     * @param string $job job string
+     * @param int $guardiansFamilyId guardians family ID
+     * @param string $class additional classes for wrapping div
+     * @return string HTML string
+     */
+    public function blazon($baseBlazon, $job = null, $guardiansFamilyId = null, $class = null)
     {
-        // cleanFix for testing
-        if (TEST_BLAZON) {
-            $dumpMsg = 'BlazonHelper.php test override';
-            if ($baseId !== 0) {
-                bdump( $dumpMsg . ' of $baseId');
-                $baseId = 0;
-            }
-            if ($overlay1 !== null) {
-                bdump( $dumpMsg . ' of $overlay1');
-                $overlay1 = 'soldat';
-            }
-            if ($overlay2 !== null) {
-                bdump( $dumpMsg . ' of $overlay2');
-                $overlay2 = 2;
-            }
-        }
+        if ( !is_string($job) ) $job = null;
+        if ( !is_int($guardiansFamilyId) ) $guardiansFamilyId = null;
 
-        if ( !is_string($overlay1) ) $overlay1 = null;
-        if ( !is_int($overlay2) ) $overlay2 = null;
-
-        return $this->createBlazon($baseId, $overlay1, $overlay2, $class);
+        return $this->createBlazonDiv($baseBlazon, $job, $guardiansFamilyId, $class);
     }
 
+    /** returns HTML string for family blazon
+     * @param int $familyId
+     * @param string $class
+     * @return string HTML string
+     */
     public function blazonFamily($familyId, $class = null)
     {
         $class .= 'family';
         return $this->blazon($familyId, '', null, $class);
     }
 
+    /** returns HTML string for followers blazon
+     * @param int $familyId
+     * @param string $job
+     * @param string $class
+     * @return string HTML string
+     */
     public function blazonFollowers($familyId, $job = '', $class = null)
     {
         $class .= 'followers';
@@ -85,7 +86,7 @@ class BlazonHelper extends AbstractHelper
         return $this->blazon($followersBlazonId, $job, $familyId, $class);
     }
 
-    private function createBlazon($baseId, $job = null, $familyId = null, $class = null)
+    private function createBlazonDiv($baseId, $job = null, $familyId = null, $class = null)
     {
         $class = ($class == null) ? 'x'.$job.$familyId : $baseId.$job.$familyId . ' ' . $class;
 
@@ -93,11 +94,11 @@ class BlazonHelper extends AbstractHelper
         $job    = $this->validateJob($job);
         $familyId = $this->validateIds($familyId);
         
-        $img = $this->createImg($baseId, $job, $familyId);
+        $img = $this->createImgElements($baseId, $job, $familyId);
         return '<div class="blazon '. $class .  '" style = " position: relative; '. $this->activeOption['divStyle'] . '">' . $img. '</div>';
     }
 
-    private function createImg($baseId, $job, $familyId)
+    private function createImgElements($baseId, $job, $familyId)
     {
         $backgroundImage2 = $backgroundImage3 = '';
         
@@ -123,43 +124,45 @@ class BlazonHelper extends AbstractHelper
         return $job;
     }
 
-    private function standards()
+    private function customizedSize($size)
     {
-        $height  = floatval(200);
-        $left    = $height*0.03;
-        $top1    = $height*0.155;
-        $height1 = $height*0.435;
-        $height2 = $height*0.45;
+        $height  = floatval(1)*$size;
+        $left    = $height * 0.03;
+        $top1    = $height * 0.155;
+        $height1 = $height * 0.435;
+        $height2 = $height * 0.45;
 
-        $this->options = array(
-            "big" => array(
-                "divStyle" => "height: " . $height . "px; width: " . $height . "px;",
-                "height1"  => "height: " . $height . "px;",
-                "overlay1" => "left: " . $left . "px; right: 0; margin: 0 auto; top: " . $top1 . "px; height: " . $height1 . "px;",
-                "overlay2" => "bottom: 0;  left: " . $left . "px; right: 0; margin: 0 auto; height: " . $height2 . "px;",
-            ),
-            "small" => array(
-                "divStyle" => "height: " . ($height /4) . "px; width: " . ($height /4) . "px;",
-                "height1"  => "height: " . ($height /4) . "px;",
-                "overlay1" => "left: " . ($left/4) . "px; right:0; margin: 0 auto; top: " . ($top1 /4) . "px; height: " . ($height1 /4) . "px;",
-                "overlay2" => "bottom: 0; left: " . ($left/4) . "px; right: 0; margin: 0 auto; height: " . ($height2 /4) . "px;",
-            ),
+        $this->activeOption = array(
+            "divStyle" => "height: " . ($height) . "px; width: " . ($height) . "px;",
+            "height1"  => "height: " . ($height) . "px;",
+            "overlay1" => "left: " . ($left) . "px; right: 0; margin: 0 auto; top: " . ($top1) . "px; height: " . ($height1) . "px;",
+            "overlay2" => "bottom: 0;  left: " . ($left) . "px; right: 0; margin: 0 auto; height: " . ($height2) . "px;",
         );
     }
 
-    private function customizedSize($size)
+    private function setStandards()
     {
-        $height  = floatval(200);
-        $left    = $height*0.03;
-        $top1    = $height*31/200;
-        $height1 = $height*87/200;
-        $height2 = $height*90/200;
-        
-        $this->activeOption = array(
-            "divStyle" => "height: " . ($height /$size) . "px; width: " . ($height /$size) . "px;",
-            "height1"  => "height: " . ($height /$size) . "px;",
-            "overlay1" => "left: " . ($left/$size) . "px; right: 0; margin: 0 auto; top: " . ($top1 /$size) . "px; height: " . ($height1 /$size) . "px;",
-            "overlay2" => "bottom: 0;  left: " . ($left/$size) . "px; right: 0; margin: 0 auto; height: " . ($height2 /$size) . "px;",
+        $this->preSetFactors = array(
+            "big" => 200, // size used to design
+            "small" => 50,
+        );
+    }
+
+    private function setJobsFromDB()
+    {
+        //todo read from db
+        $this->jobs = array (
+            'soldat' => '/img/blazons/swords.png'
+        );
+    }
+
+    private function setBlazonsFromDB()
+    {
+        //todo read from db
+        $this->blazons = array(
+            0 => array ( 'url' => '/img/blazons/shield-tross.big.png' ),
+            1 => array ( 'url' => '/img/blazons/zuLeym.png' ),
+            2 => array ( 'url' => '/img/blazons/fryschild.png'),
         );
     }
 }
