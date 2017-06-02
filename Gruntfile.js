71116485d2e9801662afcc757030c226389da527
salt <boluuuu@gmail.com>
2017-06-03 01:38:56 +0200

    add google closure compiler grunt plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
     require('jit-grunt')(grunt);
+    require('google-closure-compiler').grunt(grunt);
 
     grunt.initConfig({
         less: {
@@ -46,6 +47,32 @@ module.exports = function(grunt) {
             //     ]
             // }
         },
+        'closure-compiler': {
+            //https://github.com/google/closure-compiler-npm
+            my_target: {
+                files: {
+                    // 'public/js/output2.min.js': ['public/js/globalUsage/jquery/*.js'],
+                    // 'public/js/output.min.js': ['public/js/selectedUsage/**/*.js']
+                    'public/js/main.js': [
+                        'public/js/globalUsage/accordion/*.js',
+                        'public/js/globalUsage/loggingDesigner/*.js',
+                        'public/js/globalUsage/menu/*.js',
+                        'public/js/globalUsage/popUp/*.js',
+                    ]
+                },
+                options: {
+                    debug: true,
+                    compilation_level: 'ADVANCED',
+                    // compilation_level: 'SIMPLE',
+                    // warning_level: 'ALL',
+                    // language_in: 'ECMASCRIPT6_STRICT',
+                    // language_out: 'ECMASCRIPT5_STRICT',
+                    // language_in: 'ECMASCRIPT6',
+                    // create_source_map: 'public/js/output.min.js.map',
+                    // output_wrapper: '(function(){\n%output%\n}).call(this)\n//# sourceMappingURL=output.min.js.map'
+                }
+            }
+        },
         watch: {
             styles: {
                 files: [
@@ -62,4 +89,5 @@ module.exports = function(grunt) {
     });
 
     grunt.registerTask('default', ['less', 'watch']);
+    grunt.registerTask('build', ['closure-compiler']);
 };
